d69311174ef79216f420c59d2d0919dc537f7621
Mohammed Sohail <sohailsameja@gmail.com>
2016-10-29 15:22:26 +0530

    Fixed indentation in install script Add README.md Instructions

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # zcash-mining
+
+## Installation 
+
+```bash
+$ wget --no-check-certificate https://raw.githubusercontent.com/kamikazechaser/zcash-mining/master/install.sh
+$ sudo chmod 777 install.sh
+$ ./install.sh
+```
--- install.sh
+++ install.sh
@@ -6,18 +6,18 @@ if [ "$REPLY" != "yes" ]; then
 	echo "
 "
 else
-	echo "Installing/updating transport-https"
-	sudo apt-get install apt-transport-https
-	echo "Adding zcash master signing key to apt's truted keyring"
-	wget -qO - https://apt.z.cash/zcash.asc | sudo apt-key add -
-	echo "Adding the repository to your sources"
-	echo "deb https://apt.z.cash/ jessie main" | sudo tee /etc/apt/sources.list.d/zcash.list
-	echo "Running apt-update"
-	sudo apt-get update
-	echo "Installing zcash"
-	sudo apt-get install zcash
+    echo "Installing/updating transport-https"
+    sudo apt-get install apt-transport-https
+    echo "Adding zcash master signing key to apt's truted keyring"
+    wget -qO - https://apt.z.cash/zcash.asc | sudo apt-key add -
+    echo "Adding the repository to your sources"
+    echo "deb https://apt.z.cash/ jessie main" | sudo tee /etc/apt/sources.list.d/zcash.list
+    echo "Running apt-update"
+    sudo apt-get update
+    echo "Installing zcash"
+    sudo apt-get install zcash
     echo "Running post-install updates and setting up configurations"
-	zcash-fetch-params
+    zcash-fetch-params
     echo "Creating folder"
     mkdir -p ~/.zcash
     echo "Creating configuration file"
@@ -27,5 +27,5 @@ else
     echo "rpcuser=username" >>~/.zcash/zcash.conf
     echo "rpcpassword=`head -c 32 /dev/urandom | base64`" >>~/.zcash/zcash.conf
     echo "Zcash is now installed!"
-	echo " "
+    echo " "
 fi
